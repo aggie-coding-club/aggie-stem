0cda5971d72925a2987a0530ec78d64ad6b10dd2
Irunius <zacattack51196@gmail.com>
2017-11-19 15:06:01 -0600

    Added a title, placeholders for button presses, and working on a background image

--- password.js
+++ password.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { AppRegistry, TextInput, View, Text,StyleSheet, TouchableOpacity } from 'react-native';
+import { AppRegistry, Image, TextInput, View, Text,StyleSheet, TouchableOpacity, Alert } from 'react-native';
 
 export default class password extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {text: ''};
 	}
+	
+	_signInPressed(){
+		Alert.alert('Sign in Pressed')
+	}
+	
+	_guestPressed() {
+		Alert.alert('Guest Button Pressed')
+	}
 
 	render() {
 		return (
-			<View style={{alignItems: 'center',paddingTop:100}}>
+			<View style={{alignItems: 'center',paddingTop:50}}>
+			//TODO get background image to work
+			<Image source = {require('./background.jpg')} style = {styles.background}/>
+				<Text style = {styles.title}>
+					Aggie Stem
+				</Text>
+				//Get user's username
 				<TextInput
 					style={styles.pass}
 					placeholder = "username"
@@ -19,6 +33,7 @@ export default class password extends Component {
 				<Text style={{padding: 10, fontSize:42, textAlign: 'center'}}>
 					{this.state.text}
 				</Text>
+				//Get user's password
 				<TextInput
 					style={styles.user}
 					placeholder = "password"
@@ -28,19 +43,20 @@ export default class password extends Component {
 				<Text style={{padding: 10, fontSize:42, textAlign: 'center'}}>
 					{this.state.text}
 				</Text>
-				//Create two buttons (Sign in and Guest)
-				//No pressOn handlers yet, will need to add handlers to route sign in to main app 
-				//After validating sign in info and guest button to website
-				<TouchableOpacity>
+				//When pressed should authenticate the user and then 
+				//render then navigate to the actual application
+				<TouchableOpacity onPress = {this._signInPressed}>
 				  <View style={styles.button}>
 				    <Text style={styles.buttonText}>Sign In</Text>
 				  </View>
-			      </TouchableOpacity>
-			      <TouchableOpacity>
-			          <View style={styles.button}>
-				    <Text style={styles.buttonText}>Guest</Text>
-				  </View>
-			      </TouchableOpacity>
+			    </TouchableOpacity >
+				//When pressed take the user to the Aggie Stem Website
+				//to be given information
+			    <TouchableOpacity onPress = {this._guestPressed}>
+			     <View style={styles.button}>
+				  <Text style={styles.buttonText}>Guest</Text>
+				 </View>
+				</TouchableOpacity>
 			</View>
 	
 		
@@ -49,6 +65,11 @@ export default class password extends Component {
 }
 
 const styles = StyleSheet.create( {
+	background: {
+		flex: 1,
+		resizeMode: 'cover',
+	},
+	
 	user: {
 		color: 'white',
 		width:100,
@@ -62,6 +83,7 @@ const styles = StyleSheet.create( {
 		paddingLeft: 0,
 		width: 100,
 		height: 40,
+		margin: -10,
 		color: 'white',
 		backgroundColor: '#500000',
 		textAlign: 'center',
@@ -81,11 +103,19 @@ const styles = StyleSheet.create( {
 		borderRadius: 50,
 		height: 25,
 		alignItems: 'center',
-		backgroundColor: 'maroon'
+		backgroundColor: '#500000'
 	},
 	buttonText: {
 		padding: 5,
 		fontSize: 10,
 		color: 'white'
+	},
+	
+	title: {
+		paddingBottom: 10,
+		fontSize: 25,
+		marginBottom: 10,
+		fontWeight: "bold",
+		color: '#500000',
 	}
 });
