dafae6a76ad3355b54e67ae669ddd9bd5dbe0d47
whroberts <whr5th@gmail.com>
2018-02-08 21:38:21 -0600

    navigation works for now

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
+"use strict";
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  Alert,
+  Dimensions
+ } from 'react-native';
+ import {
+   StackNavigator,
+   TabNavigator,
+   DrawerNavigator
+ } from 'react-navigation';//imports the components from the file
+import RootNavigator from './src/navigation/Menu';
 
-AppRegistry.registerComponent('StemProject', () => App);
+//index class to actually launch to app
+const MyApp = StackNavigator({
+  Tabs: { screen: RootNavigator }
+})
+
+AppRegistry.registerComponent('StemProject', () => MyApp);
